b1c5f4058843850be97e061624e921eaf3c4305d
黒須 淳 <kurosujun@kurosunoMacBook-ea.local>
2020-03-27 11:56:19 +0900

    ~~

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,19 +2,19 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     <url>
         <loc>{{ route('home') }}/</loc>
-        <lastmod>2020-3-27</lastmod>
+        <lastmod>2020-03-27</lastmod>
     </url>
     <url>
         <loc>{{ route('studio.pref') }}</loc>
-        <lastmod>2020-3-27</lastmod>
+        <lastmod>2020-03-27</lastmod>
     </url>
     <url>
         <loc>{{ route('studio.archive') }}</loc>
-        <lastmod>2020-3-27</lastmod>
+        <lastmod>2020-03-27</lastmod>
     </url>
     <url>
         <loc>{{ route('studioRegister') }}</loc>
-        <lastmod>2020-3-27</lastmod>
+        <lastmod>2020-03-27</lastmod>
     </url>
     @foreach(\App\Studio::all() as $v)
     <url>
